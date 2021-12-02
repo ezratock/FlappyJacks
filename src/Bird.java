16b1acbdc539a5f8a7798a22a862070ad274a123
Ezra <ezra@tocks.org>
2021-12-02 00:15:52 -0500

    Created base classes

---
+++ src/Bird.java
@@ -0,0 +1,4 @@
+import javax.swing.*;
+
+public class Bird extends JComponent {
+}
---
+++ src/GamePanel.java
@@ -0,0 +1,20 @@
+import javax.swing.JPanel;
+import java.awt.*;
+import java.lang.Runnable;
+
+public class GamePanel extends JPanel implements Runnable{
+
+    static final int WIDTH = 480;
+    static final int HEIGHT = 360;
+    public void run() {
+
+    }
+
+    public void draw(Graphics g) {
+
+    }
+
+    public void move() {
+
+    }
+}
